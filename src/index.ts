868a6a1842a717796c243a260ca61cc029051fe5
7Codo - Ayoub <39974276+7codo@users.noreply.github.com>
2025-02-06 19:38:27 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
 import { PluginDefinition, setupPluginServer } from 'connery';
 import askCodaTable from "./actions/askCodaTable.js";
 
+/*
+Runtime types
+
+interface PluginRuntime {
+  name: string;
+  description?: string;
+  actions: ActionRuntime[];
+
+  findActionByKey(key: string): ActionRuntime | undefined;
+}
+
+interface ActionRuntime {
+  key: string;
+  name: string;
+  description?: string;
+  type: 'create' | 'read' | 'update' | 'delete';
+  inputParameters: InputParameterDefinition[];
+  outputParameters: OutputParameterDefinition[];
+  operation: OperationDefinition;
+  plugin: PluginRuntime;
+
+  run(input: InputObject): Promise<RunActionResponse>;
+}
+
+*/
+
 const pluginDefinition: PluginDefinition = {
   name: 'Coda',
   description: 'Connery plugin for Coda',
